20fda639cf100dccfd6fba99de9cb1bcd93da131
ajinkyapd <40497549+ajinkyapd@users.noreply.github.com>
2018-06-23 11:40:15 +0530

    confirm

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'ass'`, async(() => {
+  it(`should have as title 'FirstApp'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ass');
+    expect(app.title).toEqual('FirstApp');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ass!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to FirstApp!');
   }));
 });
